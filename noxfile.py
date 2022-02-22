2085c0088c1cae5ab6a9c5fe5351e1b82fff316a
Joris Clement <flyingdutchman@posteo.de>
2022-02-23 00:09:07 +0100

    Initial commit
    Add a tool to calculate the difference between the actually worked time
    and the wanted work time based on the report from the time tracking tool
    watson.
    
    A sample command line would be:
    watson report --from 2022/02/22 -p project --json | watson-overtime

---
+++ noxfile.py
@@ -0,0 +1,32 @@
+import nox
+from nox_poetry import session
+
+
+nox.options.sessions = "lint", "mypy", "tests"
+LOCATIONS = "watson_overtime", "noxfile.py"
+
+
+@session(python=["3.8", "3.9", "3.10"])
+def tests(session):
+    args = session.posargs or ["tests", "--cov"]
+    session.install(".")
+    session.install("coverage[toml]", "pytest", "pytest-cov")
+    session.run("pytest", *args)
+
+
+@session(python="3.9")
+def lint(session):
+    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.run("flake8", *LOCATIONS)
+
+
+@session(python="3.9")
+def black(session):
+    session.install("black")
+    session.run("black", *LOCATIONS)
+
+
+@session(python="3.9")
+def mypy(session):
+    session.install("mypy")
+    session.run("mypy", *LOCATIONS)
